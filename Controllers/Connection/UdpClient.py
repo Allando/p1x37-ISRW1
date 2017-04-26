e4bdfbbab1a377fce0bea9c7d6c523f3cf34cabc
Unknown <alla2634@edu.easj.dk>
2017-04-26 11:58:46 +0200

    added to server

--- Controllers/Connection/UdpClient.py
+++ Controllers/Connection/UdpClient.py
@@ -2,8 +2,9 @@
 
 import socket
 
+# UDP_IP = "192.168.3.250"
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+UDP_PORT = 8888
 MESSAGE = "Hello, World!"
 
 print("UDP target IP:", UDP_IP)
--- Controllers/Connection/UdpServer.py
+++ Controllers/Connection/UdpServer.py
@@ -4,14 +4,16 @@ import socket
 import serial
 
 UDP_IP = "127.0.0.1"
+# UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 
-ser = serial.Serial('/dev/tty.usbserial', 9600)
-ser.write(b'5')
+# ser = serial.Serial('/dev/tty.usbserial', 9600)
+# ser.write(b'5')
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
+address = (UDP_IP, UDP_PORT)
+sock.bind(address)
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
