986ed4ef3c139ccf3e6432f977c101c278956d7f
Mads Veelsgaard <mads581b@edu.easj.dk>
2017-04-26 13:15:27 +0200

    go

--- Controllers/Connection/UdpServer.py
+++ Controllers/Connection/UdpServer.py
@@ -3,17 +3,17 @@
 import socket
 import serial
 
-UDP_IP = "127.0.0.1"
+UDP_IP = "192.168.3.197"
 # UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+UDP_PORT = 6789
 
 # ser = serial.Serial('/dev/tty.usbserial', 9600)
 # ser.write(b'5')
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 address = (UDP_IP, UDP_PORT)
 sock.bind(address)
+
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
