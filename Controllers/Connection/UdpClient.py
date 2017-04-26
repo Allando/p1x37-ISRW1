8bb93103ffd5e5a51f163e5917229d53a85c94cc
Unknown <alla2634@edu.easj.dk>
2017-04-26 14:01:59 +0200

    osdk

--- Controllers/Connection/UdpClient.py
+++ Controllers/Connection/UdpClient.py
@@ -3,7 +3,6 @@
 import socket
 
 UDP_IP = "192.168.3.250"
-# UDP_IP = "127.0.0.1"
 UDP_PORT = 6789
 MESSAGE = "Hello, World!"
 
--- Controllers/Connection/UdpServer.py
+++ Controllers/Connection/UdpServer.py
@@ -3,18 +3,17 @@
 import socket
 import serial
 
-UDP_IP = "192.168.3.197"
-# UDP_IP = "127.0.0.1"
+UDP_IP = "192.168.3.250"
 UDP_PORT = 6789
+address = (UDP_IP, UDP_PORT)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+server.bind(("",UDP_PORT))
 
 # ser = serial.Serial('/dev/tty.usbserial', 9600)
 # ser.write(b'5')
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-address = (UDP_IP, UDP_PORT)
-sock.bind(address)
-
-
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    data, addr = server.recvfrom(1024) # buffer size is 1024 bytes
     print("received message:", data)
